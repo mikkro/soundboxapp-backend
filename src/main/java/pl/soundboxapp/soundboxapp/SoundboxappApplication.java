6c18fdc1520ae3720a5564e2dd1d186e99ca1e9a
mikolaj.krol <mikolaj.krol@onwelo.com>
2021-01-14 14:56:50 +0100

    Initial commit

---
+++ src/main/java/pl/soundboxapp/soundboxapp/SoundboxappApplication.java
@@ -0,0 +1,13 @@
+package pl.soundboxapp.soundboxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundboxappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoundboxappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/soundboxapp/soundboxapp/SoundboxappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.soundboxapp.soundboxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundboxappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
